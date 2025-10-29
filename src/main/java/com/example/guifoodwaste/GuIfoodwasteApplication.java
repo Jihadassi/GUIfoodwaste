a01f4dae88ffb7a857b25f90cc8366e9a6819fe3
Jihadassi <12345678+Jihadassi@users.noreply.github.com>
2025-10-29 12:09:43 +0100

    Initial commit

---
+++ src/main/java/com/example/guifoodwaste/GuIfoodwasteApplication.java
@@ -0,0 +1,13 @@
+package com.example.guifoodwaste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuIfoodwasteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuIfoodwasteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/guifoodwaste/GuIfoodwasteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.guifoodwaste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuIfoodwasteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
